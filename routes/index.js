a09ac3003674047f3195f06430bbcb9dec8dcaf3
Alvin Yusuf Riziq <alvinysfriz1@std.unissula.ac.id>
2023-10-24 16:11:09 +0700

    implement half routes productions

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const maintenanceRoutes = require('./maintenance');
+const productionRoutes = require('./production');
 
 router.use('/', (req, res) => res.json({ message: 'halaman routes api' }));
 router.use('/maintenance', maintenanceRoutes);
+router.use('/production', productionRoutes);
 
 module.exports = router;
