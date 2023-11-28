3a0866df6ec4fec25efb87fc63789f348bfcf1f2
Alvin Yusuf Riziq <alvinysfriz1@std.unissula.ac.id>
2023-11-28 11:38:25 +0700

    FIX: memperbarui route get target sales monthly

--- routes/target.js
+++ routes/target.js
@@ -4,6 +4,7 @@ const targetController = require('../controller/targetController');
 const router = express.Router();
 
 router.get('/', (req, res) => res.json({ message: 'router target' }));
+router.get('/get-qmp', targetController.getQmp);
 router.get('/get-monthly', targetController.getMonthly);
 router.get('/get-downtime', targetController.getTargetDowntime);
 
